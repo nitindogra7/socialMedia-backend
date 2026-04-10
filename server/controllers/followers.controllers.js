e8285a723e7226eee56dec350e83607082cc5764
Nitin Dogra <dogranitin579@gmail.com>
2026-04-11 04:09:48 +0530

    unfollow controller function

--- server/controllers/followers.controllers.js
+++ server/controllers/followers.controllers.js
@@ -1,3 +1,4 @@
+import { use } from 'react';
 import { Followers } from '../models/followers.model.js';
 export const followUser = async (req, res) => {
   try {
@@ -26,6 +27,37 @@ export const followUser = async (req, res) => {
         message: 'Already following',
       });
     }
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const unfollow = async (req, res) => {
+  try {
+    const followerId = req.user.userId;
+    const followingId = req.params.id;
+
+    if (!userFromUnfollow || !userToUnfollow) {
+      return res.status(400).json({
+        success: false,
+        message: 'User IDs required',
+      });
+    }
+
+    const unfollow = await Followers.findOneAndDelete({
+      follower: followerId,
+      following: followingId,
+    });
+
+    if (!unfollow) return res.status(404).json('users relation not found ');
+    return res.status(200).json({
+      success: true,
+      message: 'User unfollowed successfully',
+    });
+  } catch (error) {
+    console.log(error.message);
     return res.status(500).json({
       success: false,
       message: error.message,
